0db9ce403c0e97cbfff4098606a9f4c01c961bdd
Juan Martinez <jcmartinezll@gmail.com>
2021-02-27 18:54:37 -0500

    adding ccount voweels

---
+++ count_vowels.js
@@ -0,0 +1,12 @@
+// count vowels within a phrase
+// contar vocales dentro de una frase
+// Dada una frase, va retornar el número de ocurrencias de cada vocal
+// Ejemplo
+// INPUT: Hola Mundo
+// OUTPUT: a:1, e:0, i:0, o:2, u:1 
+// ó OUTPUT: hay 2 A, hay 0 e...etc..
+
+
+const prompt = require('prompt-sync')(); //npm install prompt-sync
+const input = prompt('Input a phrase ? ');
+console.log(`you've entered ${input}`)
